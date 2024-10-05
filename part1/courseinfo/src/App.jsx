efbc72a87a99bef5f4d9355d18e534c1c66d4362
Erencan Günden <erngndn35@hotmail.com>
2024-10-05 18:51:06 +0200

    v2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,12 +1,54 @@
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4},
-    {name: 'Maya', age:10},
-  ]
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+};
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age} </p>
-      <p>{friends[1].name} {friends[1].age} </p>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of exercises {total}</p>
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
