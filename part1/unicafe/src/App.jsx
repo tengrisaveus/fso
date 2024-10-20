62f31cd3c93d5b014fd0bb0ffd52171f0a7f8179
Erencan Günden <erngndn35@hotmail.com>
2024-10-20 22:18:18 +0200

    v1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [sum, setSum] = useState(0)
+
+  const calculateAverage = () => {
+    return total > 0 ? sum / total : 0
+  };
+
+  const calculatePositivePercentage = () => {
+    return total > 0 ? (good / total) * 100 : 0
+  };
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+    setSum(sum + 1)
+  };
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  };
+
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+    setSum(sum - 1)
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback </h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h1>statistics</h1>
+      {total > 0 ? (
+        <div>
+          <p>good {good} </p>
+          <p>neutral {neutral} </p>
+          <p>bad {bad} </p>
+          <p>all {total} </p>
+          <p>average {calculateAverage()} </p>
+          <p>positive {calculatePositivePercentage()}% </p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
   )
 }
 
