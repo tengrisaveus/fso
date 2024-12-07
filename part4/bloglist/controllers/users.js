fca1c9ee12cc5dd9206654861833a898f393251f
Erencan Günden <erngndn35@hotmail.com>
2024-12-07 13:28:38 +0100

    .

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,21 +3,32 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+    try{
+        const { username, name, password } = request.body
+
+        if(!password || password.length < 3){
+            return response.status(400).json({
+                error: 'Password must be at least 3 characters long',
+            })
+        }
     
-    const saltRounds = 10
-    const passwordHash = await bcryptjs.hash(password, saltRounds)
+        const saltRounds = 10
+        const passwordHash = await bcryptjs.hash(password, saltRounds)
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
+        const user = new User({
+            username,
+            name,
+            passwordHash,
+        })
+
+        const savedUser = await user.save()
+
+        response.status(201).json(savedUser)
+    }catch(error){
+        next(error)
+    }
+    
     })
-
-    const savedUser = await user.save()
-
-    response.status(201).json(savedUser)
-})
 
 usersRouter.get('/', async (request, response) => {
     const users = await User
