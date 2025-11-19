48f0a76e22bba945b005a2fc6493622eb16a35b3
Erencan Günden <erngndn35@hotmail.com>
2025-11-19 12:00:38 +0100

    feat: integrate json-server for anecdote management
    - Added json-server as a dependency and configured it to run on port 3001 with a db.json file.
    - Updated App component to fetch anecdotes from the server on mount and dispatch them to the Redux store.
    - Modified AnecdoteForm to create new anecdotes via the server and dispatch the created anecdote to the Redux store.
    - Refactored anecdoteReducer to handle setting anecdotes from the server and creating new anecdotes.
    - Created a db.json file with initial anecdotes for the json-server.
    - Implemented anecdoteService for API interactions with the json-server.

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,14 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
     
-    const addAnecdote = event => {
+    const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.createNew(content)
     dispatch(createAnecdote(content))
     dispatch(showNotification(`you created '${content}'`, 5))
   }
