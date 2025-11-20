e20621501d15a09430e991830b7424e2c00850bf
Erencan Günden <erngndn35@hotmail.com>
2025-11-20 11:42:58 +0100

    feat: initialize query-anecdotes project with React, Vite, and JSON server
    - Added package.json with dependencies for React, React Query, and JSON server
    - Created server.js to set up a JSON server with validation for anecdotes
    - Implemented main application logic in App.jsx to fetch, create, and update anecdotes
    - Added AnecdoteForm component for submitting new anecdotes
    - Created Notification component for displaying messages (currently not functional)
    - Set up requests.js for API interactions with the JSON server
    - Configured Vite for development and build processes
    - Included vite.svg for branding in the public directory

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,72 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
+
+   const updateAnecdotesMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
+  
+  const handleVote = (anecdote) => {
+    updateAnecdotesMutation.mutate({...anecdote, votes: anecdote.votes + 1 })
+  }
+ 
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1,
+    refetchOnWindowFocus: false
+  })
+ 
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError){
+    return <span>anecdote service not available due to problems in server</span>
+  }
+ 
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm onCreate={addAnecdote} />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,13 @@
+const AnecdoteForm = ({ onCreate }) => {
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
